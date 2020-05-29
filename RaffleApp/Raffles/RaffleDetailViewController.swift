54f6556e0983f3d929ac93322afdddcda7a555e8
Brandon <brandon.nimmett@gmail.com>
2020-05-29 17:30:44 +1000

    test2

--- RaffleApp/Raffles/RaffleDetailViewController.swift
+++ RaffleApp/Raffles/RaffleDetailViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class RaffleDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     var raffle : Raffle?
-    
+
     @IBOutlet var   raffleName: UILabel!
     @IBOutlet var   raffleDescription: UILabel!
     @IBOutlet var   raffleDrawDate: UILabel!
@@ -21,12 +21,12 @@ class RaffleDetailViewController: UIViewController, UIImagePickerControllerDeleg
     @IBOutlet var   rafflePrice: UILabel!
     @IBOutlet var   addImageButton: UIButton!
     @IBOutlet var   marginLabel: UILabel!
-    
+
     @IBOutlet var   sellTicketButton: UIButton!
     @IBOutlet var   drawWinnerButton: UIButton!
-    
+
     @IBOutlet var imageView: UIImageView!
-    
+
     @IBAction func AddPhotoButtonTapped(_ sender: UIButton) {
         if !UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
             print("No Photo Library Avaliable")
@@ -35,7 +35,7 @@ class RaffleDetailViewController: UIViewController, UIImagePickerControllerDeleg
         imagePicker.delegate = self
         imagePicker.sourceType = .photoLibrary
         imagePicker.allowsEditing = true
-        
+
         self.present(imagePicker, animated: true, completion: nil)
     }
 
@@ -47,11 +47,11 @@ class RaffleDetailViewController: UIViewController, UIImagePickerControllerDeleg
             dismiss(animated: true, completion: nil)
         }
     }
-    
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -69,85 +69,85 @@ class RaffleDetailViewController: UIViewController, UIImagePickerControllerDeleg
             else {
                 marginLabel.text = "Random Draw"
             }
-            
+
             print(displayRaffle.raffle_id)
-            
+
             //When backbutton is pressed, will always return to raffle list and not new ticket view.
             if let returnToRoot = navigationController?.viewControllers.first {
                 navigationController?.viewControllers = [returnToRoot, self]
             }
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         let database : SQLiteDatabase = SQLiteDatabase(databaseName:"my_database")
         raffle = database.selectRaffleByID(raffle_id: raffle?.raffle_id ?? -1)
         raffleSold.text = String(raffle?.current ?? -1)
     }
-    
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
         super.prepare(for: segue, sender: sender)
-        
+
         if segue.identifier == "EditRaffleSegue"
         {
             guard let EditRaffleViewController = segue.destination as? EditRaffleViewController else
             {
                 fatalError("Unexpected destination: \(segue.destination)")
             }
-            
+
             guard let editButton = sender as? UIBarItem else
             {
                 fatalError("Unexpected sender: \( String(describing: sender))")
             }
-            
+
             EditRaffleViewController.raffle = raffle
         }
-        
+
         if segue.identifier == "ShowNewTicketSegue"
         {
             guard let NewTicketViewController = segue.destination as? NewTicketViewController else
             {
                 fatalError("Unexpected destination: \(segue.destination)")
             }
-            
+
             guard let currentRaffle = sender as? UIButton else
             {
                 fatalError("Unexpected sender: \( String(describing: sender))")
             }
-            
+
             NewTicketViewController.raffle = raffle
         }
-    
+
         if segue.identifier == "ShowTicketTableSegue"
         {
             guard let TicketUITableViewController = segue.destination as? TicketUITableViewController else
             {
                 fatalError("Unexpected destination: \(segue.destination)")
             }
-            
+
             guard let RaffleDetailViewController = sender as? RaffleDetailViewController else
             {
                 fatalError("Unexpected sender: \( String(describing: sender))")
             }
-            
+
             TicketUITableViewController.raffle = raffle
         }
-        
+
         if segue.identifier == "DrawWinnerSegue"
         {
             guard let DrawTicketViewController = segue.destination as? DrawTicketViewController else
             {
                 fatalError("Unexpected destination: \(segue.destination)")
             }
-            
+
             guard let currentRaffle = sender as? UIButton else
             {
                 fatalError("Unexpected sender: \( String(describing: sender))")
             }
-        
+
             DrawTicketViewController.raffle = raffle
         }
-        
+         
     }
 }
